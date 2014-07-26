44e05ce838fee08c6d5fd78a8aa4ccdb1e85f965
Maxim Kalachev <maxkalachev@yandex.ru>
2014-07-26 23:05:48 +0400

    stable version

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 
 
-var ds = require('../bin/ds.js');
+var ds = require('../bin/semantic.ds.js');
 
 var list = new ds.List();
 
@@ -16,5 +16,18 @@ list.remove(3);
 
 var it = new ds.Iterator(list);
 while (it.hasNext()) {
-  console.log(it.next().getData());
+  console.log(it.next());
 }
+
+it.destroy();
+
+
+var queue = new ds.Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
